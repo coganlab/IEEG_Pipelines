ade03dafc824c32757f3b13cb282a1bbabacb367
Aaronearlerichardson <jakdaxter31@gmail.com>
2022-01-14 13:04:54 -0500

    Created the bidspath_from_layout function to switch between BIDS layouts and mne BIDSPaths

--- Python/PreProcess/preProcess.py
+++ Python/PreProcess/preProcess.py
@@ -1,17 +1,33 @@
 from bids import BIDSLayout
+from bids.layout import parse_file_entities, BIDSFile
 import mne
-import numpy as np
+import os
 from typing import Union
 from mne_bids import read_raw_bids, BIDSPath
 
-BIDS_root = r"C:\Users\ae166\Box\CoganLab\BIDS-1.0_Uniqueness_point\BIDS"
+HOME = os.path.expanduser("~")
+BIDS_root = os.path.join(HOME, r"Box\CoganLab\BIDS-1.0_Uniqueness_point\BIDS")
+#BIDS_path = BIDSPath(root=BIDS_root, suffix="ieeg")
 
 
-def line_filter(data: mne.io.Raw):
+def line_filter(data: mne.io.Raw) -> mne.io.Raw:
     #raw = mne.io.read_raw(data, preload=True)
     #raw.set_channel_types({nam: "ecog" for nam in raw.info.ch_names})
     filt = data.notch_filter(range(60, 1000, 60))
     return filt
+
+
+def bidspath_from_layout(layout: BIDSLayout, **kwargs) -> BIDSPath:
+    my_search = layout.get(**kwargs)
+    if len(my_search) >= 2:
+        raise IndexError("Search terms matched more than one file, try adding"
+                         " more search terms")
+    elif len(my_search) == 0:
+        raise IndexError("No files match your search terms")
+    my_search: BIDSFile = my_search[0]
+    entities = parse_file_entities(my_search.relpath)
+    BIDS_path = BIDSPath(root=layout.root, **entities)
+    return BIDS_path
 
 
 if __name__ == "__main__":
@@ -20,9 +36,6 @@ if __name__ == "__main__":
     for sub_id in layout.get_subjects():
         data[sub_id] = dict()
         for run in layout.get_runs():
-            #BIDS_path = BIDSPath(subject=sub_id, run=run, suffix="ieeg",
-            #                     root=BIDS_root)
-            BIDS_path = BIDSPath(**layout.get_entities(subject=sub_id, run=run,
-                                              suffix="ieeg"))
+            BIDS_path = bidspath_from_layout(layout, subject=sub_id, run=run, extension=".edf")
             raw = read_raw_bids(bids_path=BIDS_path)
             data[sub_id][run] = line_filter(raw)
