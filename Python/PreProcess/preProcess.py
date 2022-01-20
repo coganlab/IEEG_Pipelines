4a46949c4caa3b2cb28d1e8d5f1c5fc8b23fdd2c
Aaronearlerichardson <jakdaxter31@gmail.com>
2022-01-20 12:03:24 -0500

    added a multitaper and file slection control

--- Python/PreProcess/preProcess.py
+++ Python/PreProcess/preProcess.py
@@ -6,25 +6,25 @@ from typing import Union
 from mne_bids import read_raw_bids, BIDSPath
 
 HOME = os.path.expanduser("~")
-BIDS_root = os.path.join(HOME, r"Box\CoganLab\BIDS-1.0_Uniqueness_point\BIDS")
+BIDS_root = os.path.join(HOME, r"Box\CoganLab\BIDS-1.1_Uniqueness_point\BIDS")
 
 
 def line_filter(data: mne.io.Raw) -> mne.io.Raw:
     if not data.preload:
         data.load_data()
-    filt = data.notch_filter(range(60, 1000, 60))
+    filt = data.notch_filter(freqs=range(60, 1000, 60), method='spectrum_fit')
     return filt
 
 
 def bidspath_from_layout(layout: BIDSLayout, **kwargs) -> BIDSPath:
     my_search: list = layout.get(**kwargs)
     if len(my_search) >= 2:
-        raise IndexError("Search terms matched more than one file, try adding"
-                         " more search terms")
+        raise FileNotFoundError("Search terms matched more than one file, "
+                                "try adding more search terms")
     elif len(my_search) == 0:
-        raise IndexError("No files match your search terms")
+        raise FileNotFoundError("No files match your search terms")
     my_search: BIDSFile = my_search[0]
-    entities = parse_file_entities(my_search.relpath)
+    entities = my_search.get_entities()
     BIDS_path = BIDSPath(root=layout.root, **entities)
     return BIDS_path
 
@@ -33,8 +33,11 @@ if __name__ == "__main__":
     layout = BIDSLayout(BIDS_root)
     data = dict()
     for sub_id in layout.get_subjects():
+        if not sub_id == "D0029":
+            continue
         data[sub_id] = dict()
         for run in layout.get_runs():
+
             BIDS_path = bidspath_from_layout(layout, subject=sub_id, run=run,
                                              extension=".edf")
             raw = read_raw_bids(bids_path=BIDS_path)
