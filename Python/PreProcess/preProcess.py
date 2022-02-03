2cd8bfd21208955ca8d5551b6eed4beb3cdea461
Aaronearlerichardson <jakdaxter31@gmail.com>
2022-02-03 13:19:23 -0500

    fixed some naming and started mt_filtering function

--- Python/PreProcess/preProcess.py
+++ Python/PreProcess/preProcess.py
@@ -6,35 +6,49 @@ from joblib import cpu_count
 from mne_bids import read_raw_bids, BIDSPath
 import numpy as np
 from typing import Union, List, Tuple, Dict
+import matplotlib.pyplot as plt
 
 HOME = os.path.expanduser("~")
 LAB_root = os.path.join(HOME, "Box", "CoganLab")
-BIDS_root = os.path.join(HOME, "Desktop", "BIDS")
+BIDS_root = os.path.join(LAB_root, "BIDS-1.3_Phoneme_sequencing", "BIDS")
 # LAB_root, "BIDS-1.1_Uniqueness_point", "BIDS")
-TASK = "Phoneme_Sequencing"
-SUB = "D29"
-DATE_NUM = "190318"
-D_dat = os.path.join(LAB_root, "D_Data", TASK, SUB, DATE_NUM, "001"
+TASK = "Phoneme_Seq"
+SUB = "D24"
+DATE_NUM = "181026"
+D_dat_filt = os.path.join(LAB_root, "D_Data", TASK+"uencing", SUB, DATE_NUM, "001"
                      , "{}_{}_{}.cleanieeg.dat".format(SUB, TASK, DATE_NUM))
-D_dat2 = os.path.join(LAB_root, "D_Data", TASK, SUB, DATE_NUM, "001"
+D_dat_raw = os.path.join(LAB_root, "D_Data", TASK+"uencing", SUB, DATE_NUM, "001"
                       , "{}_{}_{}.ieeg.dat".format(SUB, TASK, DATE_NUM))
 
 
 def line_filter(data: mne.io.Raw) -> mne.io.Raw:
     if not data.preload:
         data.load_data()
-    filt = data.copy().notch_filter(freqs=(60, 120, 180, 240),
+    filt = data.copy().notch_filter(None,  # (60, 120, 180, 240),
                                     # method='fir',
                                     # fir_window='blackman',
                                     # pad='reflect',
                                     method='spectrum_fit',
-                                    mt_bandwidth=20.0,
+                                    mt_bandwidth=5.0,
                                     filter_length='20s',
-                                    # p_value=0.01, only used if freqs=None
+                                    p_value=0.1,  # only used if freqs=None
                                     verbose=10,
-                                    n_jobs=cpu_count())
+                                    n_jobs=cpu_count()-1)
     # make njobs 'cuda' with a gpu if method is 'fir'
     return filt
+
+
+def mt_filt(data: mne.io.Raw):  # TODO: make your own filter
+    f, ax = plt.subplots()
+    psds, freqs = mne.time_frequency.psd_multitaper(data, fmax=250, n_jobs=cpu_count(), verbose=10)
+    psds = 10 * np.log10(psds)  # convert to dB
+    psds_mean = psds.mean(0)
+    psds_std = psds.std(0)
+    ax.plot(freqs, psds_mean, color='k')
+    ax.fill_between(freqs, psds_mean - psds_std, psds_mean + psds_std,
+                    color='k', alpha=.5)
+    ax.set(title='Multitaper PSD (gradiometers)', xlabel='Frequency (Hz)',
+           ylabel='Power Spectral Density (dB)')
 
 
 def bidspath_from_layout(layout: BIDSLayout, **kwargs) -> BIDSPath:
@@ -67,7 +81,7 @@ def raw_from_layout(layout: BIDSLayout, subject: str,
 
 
 def open_dat_file(file_path: str, channels: Union[str, List[str], int],
-                  sfreq: int = 2048, types: str = "ecog") -> mne.io.RawArray:
+                  sfreq: int = 2048, types: str = "ECoG") -> mne.io.RawArray:
     with open(file_path, mode='rb') as f:
         data = np.fromfile(f, dtype="float32")
     channels.remove("Trigger")
@@ -111,7 +125,7 @@ def filt_main(layout: BIDSLayout,
 
 def figure_compare(raw: List[mne.io.Raw], labels: List[str], avg: bool = True):
     for title, data in zip(labels, raw):
-        fig = data.plot_psd(fmax=250, average=avg)
+        fig = data.plot_psd(fmax=250, average=avg, n_jobs=cpu_count())
         fig.subplots_adjust(top=0.85)
         fig.suptitle('{}filtered'.format(title), size='xx-large',
                      weight='bold')
@@ -120,12 +134,11 @@ def figure_compare(raw: List[mne.io.Raw], labels: List[str], avg: bool = True):
 
 if __name__ == "__main__":
     layout = BIDSLayout(BIDS_root)
-    raw = raw_from_layout(layout, "D0029", [1,2,3,4] )
-    raw_dat = open_dat_file(D_dat, raw.copy().ch_names)
-    raw_dat2 = open_dat_file(D_dat2, raw.copy().ch_names)
-    #raw.load_data()
-    #filt = line_filter(raw)
+    raw = raw_from_layout(layout, "D0024", [1, 2, 3, 4])
+    filt_dat = open_dat_file(D_dat_filt, raw.copy().ch_names)
+    raw_dat = open_dat_file(D_dat_raw, raw.copy().ch_names)
+    raw.load_data()
+    filt = line_filter(raw)
     # raw_dat, dat = filt_main(layout, "D0028", 1)
-    data = [raw_dat2.copy(), raw_dat.copy()]
-    figure_compare(data, ['Un',  'Correct '])
-
+    data = [raw_dat, filt_dat, raw, filt]
+    figure_compare(data, ['Un',  '', "BIDS Un", "BIDS "])
