8e6858b5ec1e2c21ac06e59b3a9d74a7ff0dcfe6
Aaronearlerichardson <jakdaxter31@gmail.com>
2023-02-23 10:28:42 -0500

    added myst_parser to extensions and added 'modules' to front page

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [  # 'myst_parser',
+extensions = ['myst_parser',
               'sphinx.ext.duration',
               'sphinx.ext.doctest',
               'sphinx.ext.autodoc',
