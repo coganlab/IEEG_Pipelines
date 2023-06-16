8bf79d618b83230fadd1cfdedce95fe3c12f36d7
dsuseendar@gmail.com <47862632+dsuseendar@users.noreply.github.com>
2023-06-16 11:36:36 -0400

    Added comments and function description

--- MATLAB/phoneme analysis/pcaLinearDecoderWrap.m
+++ MATLAB/phoneme analysis/pcaLinearDecoderWrap.m
@@ -1,90 +1,96 @@
-function [accAll,ytestAll,ypredAll,optimVarAll,aucAll, modelWeightsAll] = pcaLinearDecoderWrap(ieegSplit,labels,tw,etw,varVector,numFolds,isauc)
-% The function performs supervised PCA-LDA decoding on ephys time-series dataset;
-% Step 1: Hyperparameter optimization through nested cross-validation to
-% identify the optimal number of PC dimensions
-% Step 2: Linear discriminant decoding
-%
-% Input
-% ieegSplit (channels x trials x time): Input dataset
-% labels (trials): Input labels
-% tw: epoch time-window
-% etw: selected time-window for decoding
-% numDim: Number of PCA dimensions to include
-% numFolds: Number of folds for cross-validation
-% Output
-% accAll - overall accuracy (0 - 1)
-% ytestAll - tested labels
-% ypredall - predicted labels
+function [accAll, ytestAll, ypredAll, optimVarAll, aucAll, modelWeightsAll] = pcaLinearDecoderWrap(ieegSplit, labels, tw, etw, varVector, numFolds, isauc)
+    % Performs supervised PCA-LDA decoding on ephys time-series dataset.
+    % Step 1: Hyperparameter optimization through nested cross-validation to
+    % identify the optimal number of PC dimensions.
+    % Step 2: Linear discriminant decoding.
+    %
+    % Input:
+    % ieegSplit (channels x trials x time): Input dataset.
+    % labels (trials): Input labels.
+    % tw: Epoch time-window.
+    % etw: Selected time-window for decoding.
+    % varVector: Number of PCA dimensions to include or an array of values for hyperparameter tuning.
+    % numFolds: Number of folds for cross-validation.
+    % isauc: Flag indicating whether to compute AUC (true/false).
+    %
+    % Output:
+    % accAll: Overall accuracy (0 - 1).
+    % ytestAll: Tested labels.
+    % ypredAll: Predicted labels.
+    % optimVarAll: Optimal number of PC dimensions for each fold.
+    % aucAll: AUC values for each fold (if isauc is true).
+    % modelWeightsAll: Model weights for each fold.
 
-timeSplit = linspace(tw(1),tw(2),size(ieegSplit,3));
-timeSelect = timeSplit>=etw(1)&timeSplit<=etw(2);
-ieegModel = ieegSplit(:,:,timeSelect);
+    % Generate time split based on tw
+    timeSplit = linspace(tw(1), tw(2), size(ieegSplit, 3));
 
-accAll = 0;
-ytestAll = [];
-ypredAll = [];
-optimVarAll = [];
-aucAll = [];
-lossVectAll = [];
-modelWeightsAll = [];
-if(numFolds>0)
-    cvp = cvpartition(labels,'KFold',numFolds,'Stratify',true);
-else
-    cvp = cvpartition(labels,'LeaveOut');
-end
+    % Select time indices within the specified etw
+    timeSelect = timeSplit >= etw(1) & timeSplit <= etw(2);
+
+    % Extract the selected time window from the input dataset
+    ieegModel = ieegSplit(:, :, timeSelect);
+
+    % Initialize variables
+    accAll = 0;
+    ytestAll = [];
+    ypredAll = [];
+    optimVarAll = [];
+    aucAll = [];
+    lossVectAll = [];
+    modelWeightsAll = [];
+
+    % Perform cross-validation
+    if (numFolds > 0)
+        cvp = cvpartition(labels, 'KFold', numFolds, 'Stratify', true);
+    else
+        cvp = cvpartition(labels, 'LeaveOut');
+    end
+
     for nCv = 1:cvp.NumTestSets
-        
+        % Split the data into training and testing sets
         train = cvp.training(nCv);
         test = cvp.test(nCv);
-        ieegTrain = ieegModel(:,train,:);
-        ieegTest = ieegModel(:,test,:);
+        ieegTrain = ieegModel(:, train, :);
+        ieegTest = ieegModel(:, test, :);
         matTrain = size(ieegTrain);
-        gTrain = reshape(permute(ieegTrain,[2 1 3]),[matTrain(2) matTrain(1)*matTrain(3)]);
+        gTrain = reshape(permute(ieegTrain, [2 1 3]), [matTrain(2) matTrain(1) * matTrain(3)]);
         matTest = size(ieegTest);
-        gTest = reshape(permute(ieegTest,[2 1 3]),[matTest(2) matTest(1)*matTest(3)]);
+        gTest = reshape(permute(ieegTest, [2 1 3]), [matTest(2) matTest(1) * matTest(3)]);
 
         pTrain = labels(train);
         pTest = labels(test);
-        if(length(varVector)>1)
-            if(numFolds>0)
-                [lossVect] = scoreSelect(gTrain,pTrain,varVector,1,numFolds); % Hyper parameter tuning
+
+        if (length(varVector) > 1)
+            if (numFolds > 0)
+                % Perform hyperparameter tuning
+                [lossVect] = scoreSelect(gTrain, pTrain, varVector, 1, numFolds);
             else
-                [lossVect] = scoreSelect(gTrain,pTrain,varVector,0,numFolds);
+                [lossVect] = scoreSelect(gTrain, pTrain, varVector, 0, numFolds);
             end
 
-             lossVectAll(nCv,:) = mean(lossVect,1);
-             %phonErrorVectAll(nCv,:) = mean(phonErrorVect,1);
-            [~,optimVarId] = min(mean(lossVect,1)); % Selecting the optimal principal components
+            lossVectAll(nCv, :) = mean(lossVect, 1);
+
+            % Select the optimal principal components based on the mean loss
+            [~, optimVarId] = min(mean(lossVect, 1));
             optimVar = varVector(optimVarId);
         else
             optimVar = varVector;
         end
-%        mean(squeeze(aucVect(:,nDim,:)),1)
-        [lossMod,Cmat,yhat,aucVect,nModes,modelweights] = pcaDecodeVariance(gTrain,gTest,pTrain,...
-                       pTest,optimVar,isauc);
-    optimVarAll = [optimVarAll optimVar];
-%     size(pTest)
-     %size(aucVect)
-    ytestAll = [ytestAll pTest];
-    ypredAll = [ypredAll yhat'];
-    accAll = accAll + 1 - lossMod;
-    modelWeightsAll{nCv} = modelweights;
 
-    if(isauc)
-    aucAll = [aucAll; aucVect];
+        % Perform PCA-LDA decoding
+        [lossMod, Cmat, yhat, aucVect, nModes, modelweights] = pcaDecodeVariance(gTrain, gTest, pTrain, pTest, optimVar, isauc);
+        
+        % Store the results for this fold
+        optimVarAll = [optimVarAll optimVar];
+        ytestAll = [ytestAll pTest];
+        ypredAll = [ypredAll yhat'];
+        accAll = accAll + 1 - lossMod;
+        modelweights.optimVar = optimVarAll;
+        modelWeightsAll{nCv} = modelweights;
+
+        % Store the AUC values if isauc is true
+        if (isauc)
+            aucAll = [aucAll; aucVect];
+        end
     end
-    %CmatAll = CmatAll + Cmat;
-    end
-%     figure; 
-%     plot(varVector,lossVectAll);
-%     xlabel('Variance explained');
-%     ylabel('Classification loss');
-    
-%     figure; 
-%     plot(varVector,phonErrorVectAll);
-%     xlabel('Variance explained');
-%     ylabel('Phoneme error (bits)');
-    
-    
-    
 end
