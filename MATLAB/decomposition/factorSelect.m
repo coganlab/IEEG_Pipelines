a65eaaf944c2cb6ecf0ad3b93220e2fd3f250b38
dsuseendar@gmail.com <47862632+dsuseendar@users.noreply.github.com>
2023-06-21 17:17:41 -0400

    Removed function factorSelect

--- MATLAB/decomposition/factorSelect.m
+++
@@ -1,9 +0,0 @@
-function factorSelect(sig2analyzeAllFeature,labels,nFactors)
-    cvp = cvpartition(labels,'KFold',10,'Stratify',true);
-    linearTemplate = templateDiscriminant('DiscrimType','pseudolinear');
-    for f = 1:nFactors
-     %covMatrix = covCor(sig2analyzeAllFeature');   
-    [Lambda,Psi,T,stats,F] = factoranupdate(sig2analyzeAllFeature,1,'xtype','data','scores','wls','rotate','varimax','maxit',1000);
-    
-    end
-end
