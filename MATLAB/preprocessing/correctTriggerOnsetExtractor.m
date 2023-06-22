5ad2f24bf6dc22d93615161a62b7783379dca03f
dsuseendar@gmail.com <47862632+dsuseendar@users.noreply.github.com>
2023-06-22 15:09:17 -0400

    Added function description and comments to correct trigger onset based on correlation

--- MATLAB/preprocessing/correctTriggerOnsetExtractor.m
+++ MATLAB/preprocessing/correctTriggerOnsetExtractor.m
@@ -1,41 +1,65 @@
-function [trigOnsUpdate,micSplitNew] = correctTriggerOnsetExtractor(trigOns,timitPath,audioPathUsed,micSplit,fsMic)
-% trigOns - extracted Trigger Onsets (1 x #triggers) in seconds
-% timitPath - file Path to timit database
-% audioPath - cell matrix {1 x #triggers} - file path to individual trials
-micDel =[]; trigOnsUpdate = zeros(1,length(trigOns));
-micSplitNew = [];
-for iSound = 1:length(audioPathUsed)
-    %iSound
-    %[timitPath audioPathUsed{iSound}]
-    [audioTimit,fsAudio] = audioread([timitPath audioPathUsed{iSound}]);
-    size(zscore(resample(audioTimit,fsMic,fsAudio)))
-    if(size(audioTimit,2)>1)
-        audioTimit = audioTimit(:,1);
-    end
-    
-    [mcal,mdal,micDelTemp] = alignsignals(zscore(resample(audioTimit,fsMic,fsAudio)),zscore(micSplit(iSound,:)));
-    micDelTemp
-    micDel(iSound) = micDelTemp;
-    micTemp = resample(audioTimit,fsMic,fsAudio);
-    micTemp = micTemp';
-    
-    if(size(micSplit,2)> length(micTemp))
-        micSplitNew(iSound,:) = [micTemp zeros(1, size(micSplit,2) - length(micTemp))];
-    else        
-        micSplitNew(iSound,:) = micTemp(1:size(micSplit,2));
-    end
-%     figure;
-%     plot(zscore(resample(audioTimit{iSound},fsMic,fsAudio)));
-%     hold on;
-%     plot(zscore(micSplit(iSound,:))); 
-    trigOnsUpdate(iSound) = (trigOns(iSound) + micDel(iSound)/fsMic);
-    
-end
-% for iSound = 1:20
-% figure;
-%     plot(zscore(resample(audioTimit{iSound},fsMic,fsAudio)));
-%     hold on;
-%     plot(zscore(micSplitNew(iSound,:))); 
-% end
+% correctTriggerOnsetExtractor - Correct trigger onset extraction using audio alignment.
+%
+% Syntax: [trigOnsUpdate, micSplitNew] = correctTriggerOnsetExtractor(trigOns, timitPath, audioPathUsed, micSplit, fsMic)
+%
+% Inputs:
+%   trigOns       - Extracted trigger onsets (1 x #triggers) in seconds
+%   timitPath     - File path to TIMIT database
+%   audioPathUsed - Cell matrix {1 x #triggers} - File path to individual trials
+%   micSplit      - Microphone split signals (matrix with size #triggers x samples)
+%   fsMic         - Sampling frequency of the microphone split signals
+%
+% Outputs:
+%   trigOnsUpdate - Updated trigger onsets after correction
+%   micSplitNew   - Corrected microphone split signals after alignment
+%
+% Example:
+%   trigOns = [0.5, 1.2, 2.8];
+%   timitPath = 'path/to/timit';
+%   audioPathUsed = {'trial1.wav', 'trial2.wav', 'trial3.wav'};
+%   micSplit = [mic1; mic2; mic3]; % matrix of microphone split signals
+%   fsMic = 44100;
+%   [trigOnsUpdate, micSplitNew] = correctTriggerOnsetExtractor(trigOns, timitPath, audioPathUsed, micSplit, fsMic);
+%
 
+
+function [trigOnsUpdate, micSplitNew] = correctTriggerOnsetExtractor(trigOns, timitPath, audioPathUsed, micSplit, fsMic)
+    % Initialize variables for storing microphone delays and updated trigger onsets
+    micDel = [];
+    trigOnsUpdate = zeros(1, length(trigOns));
+    micSplitNew = [];
+    
+    % Loop over each trigger sound
+    for iSound = 1:length(audioPathUsed)
+        % Read audio from TIMIT database
+        [audioTimit, fsAudio] = audioread([timitPath audioPathUsed{iSound}]);
+        
+        % Resample and normalize the audio signal
+        audioTimit = zscore(resample(audioTimit, fsMic, fsAudio));
+        
+        % Check if the audio signal has multiple channels and keep only the first channel
+        if size(audioTimit, 2) > 1
+            audioTimit = audioTimit(:, 1);
+        end
+        
+        % Perform signal alignment between audio and microphone split signal
+        [mcal, mdal, micDelTemp] = alignsignals(audioTimit, zscore(micSplit(iSound, :)));
+        
+        % Store the microphone delay
+        micDel(iSound) = micDelTemp;
+        
+        % Resample the aligned audio signal
+        micTemp = resample(audioTimit, fsMic, fsAudio);
+        micTemp = micTemp';
+        
+        % Adjust the length of the microphone split signal to match the aligned audio signal
+        if size(micSplit, 2) > length(micTemp)
+            micSplitNew(iSound, :) = [micTemp zeros(1, size(micSplit, 2) - length(micTemp))];
+        else
+            micSplitNew(iSound, :) = micTemp(1:size(micSplit, 2));
+        end
+        
+        % Update the trigger onset with the microphone delay
+        trigOnsUpdate(iSound) = trigOns(iSound) + micDel(iSound) / fsMic;
+    end
 end
