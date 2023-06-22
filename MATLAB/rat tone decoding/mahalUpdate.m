9b395220000e2f364d986c59ffc8aaf2c2f962cf
dsuseendar@gmail.com <47862632+dsuseendar@users.noreply.github.com>
2023-06-22 16:50:09 -0400

    Removing repetitive functions

--- MATLAB/rat tone decoding/mahalUpdate.m
+++
@@ -1,8 +0,0 @@
-function dist = mahalUpdate(noise,signal,sigma)
-meanN = mean(noise,1)';
-% sigmaInv = inv(sigma);
-nTrials = size(noise,1);
-for tr = 1:nTrials
-    dist(tr) = sqrt((signal(tr,:)'-meanN)'/sigma*(signal(tr,:)'-meanN));
-end
-end
