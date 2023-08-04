b2e417ef4dbf979a954055a12a8bd2a40ce053b4
Aaron Earle-Richardson <Jakdaxter31@gmail.com>
2023-08-04 12:06:32 -0400

    updated testing organization
    Signed-off-by: Aaron Earle-Richardson <Jakdaxter31@gmail.com>

--- ieeg/_tests/test_ieeg.py
+++ ieeg/_tests/test_ieeg.py
@@ -3,12 +3,10 @@ import os
 import mne
 import numpy as np
 import pytest
-import scipy
 from bids import BIDSLayout
 from mne.io import BaseRaw
 from mne_bids import BIDSPath
 
-from ieeg.calc.stats import mean_diff
 from ieeg.io import raw_from_layout
 
 bids_root = mne.datasets.epilepsy_ecog.data_path()
@@ -105,53 +103,3 @@ def test_trial_outlier(outliers, n_out):
     trials = trial_ieeg(seeg, 'Response', (-1, 1))
     outs = outliers_to_nan(trials, outliers)
     assert np.isnan(outs._data).sum() == n_out
-
-
-@pytest.mark.parametrize("func, expected", [
-    (mean_diff, np.arange(38, 56)),
-    (scipy.stats.f_oneway, np.arange(46, 50)),
-    (scipy.stats.ttest_ind, np.arange(38, 56))
-])
-def test_stats(func, expected):
-    from ieeg.navigate import trial_ieeg
-    from ieeg.calc import stats
-
-    out = []
-    for epoch, t in zip(('Fixation', 'Response'), ((-0.3, 0), (-0.1, 0.2))):
-        times = [None, None]
-        times[0] = t[0] - 0.5
-        times[1] = t[1] + 0.5
-        trials = trial_ieeg(seeg, epoch, times, preload=True)
-        out.append(trials)
-    resp = out[1]
-    resp.decimate(10)
-    base = out[0]
-    base.decimate(10)
-
-    mask = stats.time_perm_cluster(resp.copy()._data[:, 78:79],
-                                   base.copy()._data[:, 78:79], 0.01,
-                                   stat_func=func, n_perm=4000)
-    assert np.mean(mask[:, expected]) > 0.8
-
-
-def test_stats_wavelet():
-    from ieeg.navigate import trial_ieeg, outliers_to_nan
-    from ieeg.timefreq.utils import wavelet_scaleogram, crop_pad
-    from ieeg.calc import stats
-
-    out = []
-    for epoch, t in zip(('Fixation', 'Response'), ((-0.3, 0), (-0.1, 0.2))):
-        times = [None, None]
-        times[0] = t[0] - 0.5
-        times[1] = t[1] + 0.5
-        trials = trial_ieeg(seeg, epoch, times, preload=True, picks=[78])
-        outliers_to_nan(trials, 7)
-        spec = wavelet_scaleogram(trials, n_jobs=-2, decim=20)
-        crop_pad(spec, "0.5s")
-        out.append(spec)
-    resp = out[1]
-    base = out[0]
-
-    mask = stats.time_perm_cluster(resp._data, base._data, 0.05, n_perm=3000)
-
-    assert np.all(mask[2:7, 11:12])
