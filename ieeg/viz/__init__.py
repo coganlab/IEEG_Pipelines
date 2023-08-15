d7c7bfe3470cd1dab74400c767dbe186121e86de
Aaron Earle-Richardson <Jakdaxter31@gmail.com>
2023-08-14 23:10:08 -0400

    duck importing
    Signed-off-by: Aaron Earle-Richardson <Jakdaxter31@gmail.com>

--- ieeg/viz/__init__.py
+++ ieeg/viz/__init__.py
@@ -1,11 +1,13 @@
+from mne.utils import logger
+
+
 def _qt_backend():
     """Set the backend to Qt5Agg"""
     import matplotlib
-    if matplotlib.get_backend() not in ['Qt5Agg', 'headless']:
-        try:
-            matplotlib.use('Qt5Agg')
-        except ImportError:
-            pass
+    try:
+        matplotlib.use('Qt5Agg')
+    except ImportError:
+        logger.warn("Qt5Agg backend not available, using default backend")
 
 
 _qt_backend()
