65cd4d982d3a48163a56c18a4e70df0f9d68d322
Aaronearlerichardson <jakdaxter31@gmail.com>
2023-11-07 17:27:02 -0500

    added kording lab code

---
+++ ieeg/decoding/__init__.py
@@ -0,0 +1,10 @@
+## Orginal code found here:
+# https://github.com/KordingLab/Neural_Decoding
+
+from .decoders import (WienerFilterDecoder, WienerCascadeDecoder,
+                       KalmanFilterDecoder, DenseNNDecoder, SimpleRNNDecoder,
+                       GRUDecoder, LSTMDecoder, XGBoostDecoder, SVRDecoder,
+                       NaiveBayesDecoder)
+from .metrics import get_R2, get_rho
+from .preprocessing_funcs import (bin_output, bin_spikes,
+                                  get_spikes_with_history)
