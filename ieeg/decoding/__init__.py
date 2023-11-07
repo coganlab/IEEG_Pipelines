c97fe3e9987437eff5b82a18eba1681caac5d21a
Aaronearlerichardson <jakdaxter31@gmail.com>
2023-11-07 17:51:03 -0500

    refactored and added decoding

--- ieeg/decoding/__init__.py
+++ ieeg/decoding/__init__.py
@@ -4,7 +4,7 @@
 from .decoders import (WienerFilterDecoder, WienerCascadeDecoder,
                        KalmanFilterDecoder, DenseNNDecoder, SimpleRNNDecoder,
                        GRUDecoder, LSTMDecoder, XGBoostDecoder, SVRDecoder,
-                       NaiveBayesDecoder)
+                       NaiveBayesDecoder, PcaLdaClassification)
 from .metrics import get_R2, get_rho
 from .preprocessing_funcs import (bin_output, bin_spikes,
                                   get_spikes_with_history)
