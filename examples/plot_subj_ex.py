8d2c2a207fc326265c476cedfb721135deaddb03
Aaron Earle-Richardson <Jakdaxter31@gmail.com>
2023-12-19 10:29:20 -0500

    fixed plotting

--- examples/plot_subj_ex.py
+++ examples/plot_subj_ex.py
@@ -13,4 +13,14 @@ sample_path = mne.datasets.sample.data_path()
 raw = mne.io.read_raw(misc_path / 'seeg' / 'sample_seeg_ieeg.fif')
 subjects_dir = os.path.join(sample_path, 'subjects')
 
-plot_subj(raw, subjects_dir, labels_every=None)
+# %% make matching subject directory for subject if it doesn't exist
+if not os.path.exists(os.path.join(subjects_dir, "1")):
+    os.symlink(os.path.join(subjects_dir, 'sample'),
+               os.path.join(subjects_dir, '1'))
+
+# %% load the data
+
+raw = mne.io.read_raw(misc_path / "seeg" / "sample_seeg_ieeg.fif")
+
+# %% plot the data
+plot_subj(raw, subj_dir=subjects_dir, show=False)
