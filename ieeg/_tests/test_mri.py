ddd5b66a6dcaf0d5b9484cdf831064f934e40dd4
Aaron Earle-Richardson <Jakdaxter31@gmail.com>
2024-01-10 13:23:28 -0500

    added test to plot_on_average

--- ieeg/_tests/test_mri.py
+++ ieeg/_tests/test_mri.py
@@ -7,11 +7,18 @@ sample_path = mne.datasets.sample.data_path()
 subjects_dir = sample_path / "subjects"
 os.environ["MNE_3D_OPTION_ANTIALIAS"] = "false"
 
+if not os.path.exists(os.path.join(subjects_dir, "1")):
+    os.symlink(os.path.join(subjects_dir, 'sample'),
+               os.path.join(subjects_dir, '1'))
+raw = mne.io.read_raw(misc_path / "seeg" / "sample_seeg_ieeg.fif")
+
 
 def test_plot_seeg():
-    if not os.path.exists(os.path.join(subjects_dir, "1")):
-        os.symlink(os.path.join(subjects_dir, 'sample'),
-                   os.path.join(subjects_dir, '1'))
     from ieeg.viz.mri import plot_subj
-    raw = mne.io.read_raw(misc_path / "seeg" / "sample_seeg_ieeg.fif")
     plot_subj(raw, subj_dir=subjects_dir, show=False)
+
+
+def test_plot_avg():
+    from ieeg.viz.mri import plot_on_average
+    plot_on_average(raw, subj_dir=subjects_dir, show=False, rm_wm=False,
+                    hemi='both')
