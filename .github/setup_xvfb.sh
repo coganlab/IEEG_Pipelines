eae3e80b02c1cd6a2a2f8084532a32015ad62c8f
Aaron Earle-Richardson <Jakdaxter31@gmail.com>
2024-01-10 11:28:49 -0500

    xvfb is running

--- .github/setup_xvfb.sh
+++ .github/setup_xvfb.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 # set up the headless display for pyvista off-screen rendering on example plots
 set -x
-apt-get update
-apt-get install libgl1-mesa-glx xvfb -y
-apt-get install libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xfixes0 libopengl0 libegl1 libosmesa6 mesa-utils libxcb-shape0 libx
 export DISPLAY=:99.0
 export PYVISTA_OFF_SCREEN=True
 which Xvfb
@@ -13,5 +10,6 @@ sleep 3
 set +x
 
 # This also includes the libraries necessary for PyQt5/PyQt6
-apt-get install xvfb libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xfixes0 libopengl0 libegl1 libosmesa6 mesa-utils libxcb-shape0 libxcb-cursor0 -y
-/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -screen 0 1400x900x24 -ac +extension GLX +render -noreset
+#apt-get update
+#apt-get install xvfb libgl1-mesa-glx libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xfixes0 libopengl0 libegl1 libosmesa6 mesa-utils libxcb-shape0 libx libxcb-cursor0 -y
+#/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -screen 0 1400x900x24 -ac +extension GLX +render -noreset
