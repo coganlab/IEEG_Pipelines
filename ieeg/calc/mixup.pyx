b5f5ff646d981bdc7389fc5051468577686aed5a
Aaron Earle-Richardson <Jakdaxter31@gmail.com>
2024-04-09 17:30:03 -0400

    fixed mixup.pyx

--- ieeg/calc/mixup.pyx
+++ ieeg/calc/mixup.pyx
@@ -1,56 +1,64 @@
 import numpy as np
 cimport numpy as cnp
 cimport cython
-from libc.stdlib cimport rand, RAND_MAX, srand
-from libc.math cimport sqrt
+from libc.stdlib cimport rand, srand, malloc
+from libc.math cimport sqrt, isnan
 from numpy.random cimport bitgen_t
-from numpy.random import SFC64
+from numpy.random import SFC64, Generator
 from cpython.pycapsule cimport PyCapsule_IsValid, PyCapsule_GetPointer
 
-DTYPE = np.float64
-ctypedef cnp.float64_t DTYPE_t
-ctypedef cnp.uint8_t BOOL_t
-ctypedef cnp.intp_t INTP_t
+
 cnp.import_array()
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
-cdef inline void mixup2d(DTYPE_t[:, :] arr, DTYPE_t alpha=1.0):
-    cdef Py_ssize_t i, n_nan, row, j
-    cdef cnp.ndarray[BOOL_t, ndim=1, cast=True] wh
-    cdef INTP_t [::1] non_nan_rows, nan_rows
-    cdef DTYPE_t [::1] lam
-    cdef INTP_t x1, x2
+cdef inline void mixup2d(double[:, :] arr, const Py_ssize_t x, const Py_ssize_t y,
+                         double[::1] lam, const Py_ssize_t n_nan) noexcept nogil:
+    cdef Py_ssize_t i, row, j, k = 0, x1, x2, *non_nan_rows, *nan_rows
+    cdef int *wh = <int *>malloc(x * sizeof(int))
+
+    non_nan_rows = <Py_ssize_t*>malloc((x - n_nan) * sizeof(Py_ssize_t))
+    nan_rows = <Py_ssize_t*>malloc(n_nan * sizeof(Py_ssize_t))
 
     # Get indices of rows with NaN values
-    wh = np.isnan(arr).any(axis=1)
-    non_nan_rows = np.nonzero(~wh)[0]
-    nan_rows = np.nonzero(wh)[0]
-    n_nan = arr.shape[0] - non_nan_rows.shape[0]
+    for i in range(x):
+        for j in range(y):
+            if isnan(arr[i, j]):
+                wh[i] = 1
+                nan_rows[i - k] = i
+                break
+        else:
+            wh[i] = 0
+            non_nan_rows[k] = i
+            k += 1
 
-    # get beta distribution parameters
-    if alpha > 0:
-        lam = np.random.beta(alpha, alpha, size=n_nan).astype(DTYPE)
-    else:
-        lam = np.ones(n_nan, dtype=DTYPE)
+    for i in range(n_nan):
+        row = nan_rows[i]
+        x1 = non_nan_rows[rand() % k]
+        x2 = non_nan_rows[rand() % k]
+        while x1 == x2:
+            x2 = non_nan_rows[rand() % k]
+        for j in range(y):
+            arr[row, j] = lam[i] * arr[x1, j] + (1 - lam[i]) * arr[x2, j]
 
-    with nogil:
-        for i in range(n_nan):
-            row = nan_rows[i]
-            x1 = non_nan_rows[rand() % non_nan_rows.shape[0]]
-            x2 = non_nan_rows[rand() % non_nan_rows.shape[0]]
-            while x1 == x2:
-                x2 = non_nan_rows[rand() % non_nan_rows.shape[0]]
-            for j in range(arr.shape[1]):
-                arr[row, j] = lam[i] * arr[x1, j] + (1 - lam[i]) * arr[x2, j]
-
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+cdef inline void mixup3d(double[:, :, :] arr, const Py_ssize_t x, const Py_ssize_t y,
+                         const Py_ssize_t z, double[:, ::1] lam, Py_ssize_t[::1] n_nan) noexcept nogil:
+    for i in range(x):
+        mixup2d(arr[i], y, z, lam[i, :n_nan[i]], n_nan[i])
 
 @cython.boundscheck(False)
 cpdef void mixupnd(cnp.ndarray arr, int obs_axis, float alpha=1.0,
                    int seed=-1):
     cdef cnp.ndarray arr_in
     cdef RNG rng
+    cdef double[:, ::1] lam
+    cdef Py_ssize_t[::1] n_nan
+    cdef Py_ssize_t i
+    global rng
 
     # create a view of the array with the observation axis in the second to
     # last position
@@ -62,13 +70,29 @@ cpdef void mixupnd(cnp.ndarray arr, int obs_axis, float alpha=1.0,
     if seed == -1:
         rng = RNG()
         seed = rng.rng.next_uint32(rng.rng.state)
+    elif seed >= 0:
+        rng = RNG(seed)
 
     if seed != -2:
         srand(seed)
 
     if arr.ndim == 2:
-        mixup2d(arr_in, alpha)
-    elif arr.ndim > 2:
+        # get beta distribution parameters
+        n_nan = np.isnan(arr_in).any(axis=1).sum(dtype=np.intp)[None]
+        if alpha > 0:
+            lam = rng.generator.beta(alpha, alpha, size=(1, n_nan[0]))
+        else:
+            lam = np.ones((1, n_nan))
+        mixup2d(arr_in, arr_in.shape[0], arr_in.shape[1], lam[0], n_nan[0])
+    elif arr.ndim == 3:
+        n_nan = np.isnan(arr_in).any(axis=2).sum(axis=1, dtype=np.intp)
+        if alpha > 0:
+            lam = rng.generator.beta(alpha, alpha, size=(arr_in.shape[0], np.max(n_nan)))
+        else:
+            lam = np.ones((arr_in.shape[0], np.max(n_nan)))
+
+        mixup3d(arr_in, arr_in.shape[0], arr_in.shape[1], arr_in.shape[2], lam, n_nan)
+    elif arr.ndim > 3:
         for i in range(arr_in.shape[0]):
             # Ensure that the last two dimensions are free
             mixupnd(arr_in[i], -2, alpha, -2)
@@ -131,6 +155,7 @@ cpdef void normnd(cnp.ndarray arr, int obs_axis=-1):
 cdef class RNG:
     cdef bitgen_t *rng
     cdef object bit_generator
+    cdef object generator
 
     def __cinit__(self, int seed=-1):
         cdef const char *capsule_name = "BitGenerator"
@@ -138,6 +163,7 @@ cdef class RNG:
             self.bit_generator = SFC64()
         else:
             self.bit_generator = SFC64(seed=seed)
+        self.generator = Generator(self.bit_generator)
         capsule = self.bit_generator.capsule
         if not PyCapsule_IsValid(capsule, capsule_name):
             raise ValueError("Invalid pointer to anon_func_state")
