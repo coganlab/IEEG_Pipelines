791c3b86f7204335acb76e0c3848285cbf8ca378
Aaron Earle-Richardson <Jakdaxter31@gmail.com>
2024-04-23 13:06:22 -0400

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -303,7 +303,7 @@ def linkcode_resolve(domain, info):
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['tests']
+exclude_patterns = ['tests', 'setup']
 
 
 # -- Options for HTML output -------------------------------------------------
