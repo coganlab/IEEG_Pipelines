1bddc32793858d693e41a54238375c18dc58aa48
Raymond Xiong <raymx2004@gmail.com>
2024-04-30 12:31:11 -0400

    Change test_mri to pytest style.

--- tests/test_mri.py
+++ tests/test_mri.py
@@ -13,16 +13,43 @@ if not os.path.exists(os.path.join(subjects_dir, "1")):
                os.path.join(subjects_dir, '1'))
 raw = mne.io.read_raw(misc_path / "seeg" / "sample_seeg_ieeg.fif")
 
+import pytest
 
-def test_plot_seeg():
+@pytest.mark.parametrize(
+    'args, expected', [
+    ({'inst': raw, 'subj_dir': subjects_dir, 'show': False}, None),
+    ({'inst': raw, 'subj_dir': subjects_dir, 'show': False, 'labels_every': 1}, None),
+    ({'inst': raw, 'subj_dir': subjects_dir, 'show': False, 'hemi': 'split'}, None),
+    ({'inst': raw, 'subj_dir': subjects_dir, 'show': False, 'hemi': 'lh'}, None),
+    ({'inst': raw, 'subj_dir': subjects_dir, 'show': False, 'size': 0.2}, None),
+    ({'inst': raw, 'subj_dir': subjects_dir, 'show': False, 'background': 'red'}, None),
+    ({'inst': raw, 'subj_dir': subjects_dir, 'show': False, 'transparency': 0.2}, None),
+    ({'inst': raw, 'subj_dir': subjects_dir, 'show': False, 'color': (1, 0, 0)}, None),
+    # Generates a plot when show=True but yields error
+    ({'inst': raw, 'subj_dir': subjects_dir, 'show': False, 'picks':[0,1,2,3,4]}, None),
+    ({'inst': raw, 'subj_dir': subjects_dir, 'show': False, 'hemi': 'rh'}, None),
+    # Fails ompletely
+    ({'inst': raw, 'subj_dir': subjects_dir, 'show': False, 'no_wm': True}, None),
+])
+def test_plot_seeg_inst(args, expected):
     from ieeg.viz.mri import plot_subj
-    plot_subj(raw, subj_dir=subjects_dir, show=False)
+    plot_subj(**args)
 
-
-def test_plot_avg():
+@pytest.mark.parametrize(
+    'args, expected', [
+    ({'sigs': raw, 'subj_dir': subjects_dir, 'show': False}, None),
+    ({'sigs': raw, 'subj_dir': subjects_dir, 'show': False, 'label_every': 1}, None),
+    ({'sigs': raw, 'subj_dir': subjects_dir, 'show': False, 'hemi': 'both'}, None),
+    ({'sigs': raw, 'subj_dir': subjects_dir, 'show': False, 'hemi': 'split'}, None),
+    ({'sigs': raw, 'subj_dir': subjects_dir, 'show': False, 'hemi': 'lh'}, None),
+    ({'sigs': raw, 'subj_dir': subjects_dir, 'show': False, 'size': 0.2}, None),
+    ({'sigs': raw, 'subj_dir': subjects_dir, 'show': False, 'background': 'red'}, None),
+    ({'sigs': raw, 'subj_dir': subjects_dir, 'show': False, 'transparency': 0.2}, None),
+    ({'sigs': raw, 'subj_dir': subjects_dir, 'show': False, 'color': (1, 0, 0)}, None),
+])
+def test_plot_avg(args, expected):
     from ieeg.viz.mri import plot_on_average
-    plot_on_average(raw, subj_dir=subjects_dir, show=False, rm_wm=False,
-                    hemi='both')
+    plot_on_average(**args)
 
 
 # if on windows, finalize closing the vtk window
